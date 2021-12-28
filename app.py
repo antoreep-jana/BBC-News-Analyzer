ae793a7a078bb92cd95b5826348ff0d0d2533a63
Antoreep Jana <antoreepjana@gmail.com>
2021-12-28 15:33:04 +0530

    updated data & app.py

--- app.py
+++ app.py
@@ -76,7 +76,9 @@ if mode == "Simple":
 	elif option == "Entity Extraction":
 
 		#text = "This is an Apple watch. Costed about $100 on 21st June, 2022."
-		
+		text = """
+		This sentence has 10 words and we are testing it.
+		"""*30		
 
 		from named_entity_recognizer import NER 
 
