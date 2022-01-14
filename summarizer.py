9689b356c38db790c4b0f6d8d0c6f45e5f83abd6
Antoreep Jana <antoreepjana@gmail.com>
2022-01-14 20:43:16 +0530

    small fixes

--- summarizer.py
+++ summarizer.py
@@ -11,9 +11,9 @@ import streamlit as st
 @st.cache(allow_output_mutation = True, show_spinner = False, max_entries = 6)
 def get_models():
 	#model = AutoModelForSeq2SeqLM.from_pretrained('t5-base')
-	model = AutoModelForSeq2SeqLM.from_pretrained('bart-base')
+	model = AutoModelForSeq2SeqLM.from_pretrained('facebook/bart-base')
 	#tokenizer = AutoTokenizer.from_pretrained('t5-base')
-	tokenizer = AutoTokenizer.from_pretrained('bart-base')
+	tokenizer = AutoTokenizer.from_pretrained('facebook/bart-base')
 	return model, tokenizer
 
 class Summarizer:
